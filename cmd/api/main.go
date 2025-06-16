1da2613ca4b326c0afa2535e2bbbfb06996911f7
Arismonx <arismonx@gmail.com>
2025-06-16 19:20:02 +0700

    test: test config in path cmd/api/main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Arismonx/van-booking-system/config"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	conf := config.LoadConfig() // test config
+	fmt.Println(conf)
 }
